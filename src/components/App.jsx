d3b8d1dc37da848eb08682e63964ff117c151cdc
dire <example@gmail.com>
2023-03-28 21:46:03 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -44,8 +44,6 @@ export function App() {
     setFilter(e.currentTarget.value);
   };
 
-
-
   useEffect(() => {
 		localStorage.setItem("contacts", JSON.stringify(contacts));
   }, [contacts]);
@@ -60,91 +58,11 @@ export function App() {
     <div>
       <ContactForm onSubmit={addContact} onResetArr={contacts} />
 
-      {/* <SearchFilter
-			OnChangeFilter={this.changeFilter}
-			valueFilter={this.state.filter}
-		  /> */}
+      <SearchFilter
+			OnChangeFilter={changeFilter}
+			valueFilter={filter}
+		  />
       <ContactList contacts={filterContacts()} remove={hendeleClickDelete} />
     </div>
   );
 }
-
-// export class App extends Component {
-//   state = {
-//     contacts: [
-//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-//     ],
-//     filter: '',
-//   };
-
-//   addContact = ({ name, number }) => {
-//     const newContact = {
-//       id: nanoid(),
-//       name,
-//       number,
-//     };
-
-//     if(this.state.contacts.find(contact => newContact.name.toLowerCase() === contact.name.toLowerCase())){
-// 		alert(`${name} is already in contacts`)
-// 	} else{
-// 		this.setState(prevState => ({contacts: [newContact, ...prevState.contacts]}))
-// 	}};
-
-//   hendeleClickDelete = evt => {
-//     this.setState({
-//       contacts: this.state.contacts.filter(contact => contact.id !== evt),
-//     });
-//   };
-
-//   changeFilter = e => {
-//     this.setState({ filter: e.currentTarget.value });
-//   };
-
-//   resetForm = () => {
-//     this.setState({
-//       name: '',
-//       number: ''
-//     })
-//   };
-
-//   componentDidMount(){
-// 	const contact = localStorage.getItem('contacts');
-//     const parseContacs = JSON.parse(contact);
-// 	if(parseContacs){
-// 		this.setState({contacts: parseContacs})
-// 	}
-//   }
-
-//   componentDidUpdate(_, prevState) {
-// 	if(prevState !== this.state.contacts){
-// 		localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-// 	}
-//   }
-
-//   filterContacts = () => {
-//     return this.state.contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
-//     );
-//   };
-
-//   render() {
-//     return (
-//       <div>
-
-//         <ContactForm onSubmit={this.addContact} onResetArr={this.state.contacts}/>
-
-//         {/* <SearchFilter
-//           OnChangeFilter={this.changeFilter}
-//           valueFilter={this.state.filter}
-//         /> */}
-//         <ContactList
-//           contacts={this.filterContacts()}
-//           remove={this.hendeleClickDelete}
-//         />
-//       </div>
-//     );
-//   }
-// }
